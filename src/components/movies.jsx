49c4e8b8a96844671562eddf4728668a176b34b8
thang truong <thangtruong1808@gmail.com>
2022-01-31 16:39:39 +1100

    Exercise vidly

---
+++ src/components/movies.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+	state = {
+		movies: getMovies(),
+	};
+
+	handleDelete = (movie) => {
+		const movies_filtered = this.state.movies.filter(
+			(m) => m._id !== movie._id
+		);
+		this.setState({ movies: movies_filtered });
+	};
+
+	render() {
+		return (
+			<table className="table">
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>Genre</th>
+						<th>Stock</th>
+						<th>Rate</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.state.movies.map((movie) => (
+						<tr key={movie._id}>
+							<td>{movie.title}</td>
+							<td>{movie.genre.name}</td>
+							<td>{movie.numberInStock}</td>
+							<td>{movie.dailyRentalRate}</td>
+							<td>
+								<button
+									onClick={() => this.handleDelete(movie)}
+									className="btn btn-danger btn-sm"
+								>
+									Delete
+								</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		);
+	}
+}
+
+export default Movies;
