315095e0ee17036b3c486ec5bfaf262bb93b6918
thang truong <thangtruong1808@gmail.com>
2022-01-31 17:59:57 +1100

    update getbadgeclassess git status
    clear
    clear
    git push

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -12,19 +12,24 @@ class Movies extends Component {
 		);
 		this.setState({ movies: movies_filtered });
 	};
-
+	getBadgeClasess() {
+		let classes = "badge m-2 bg-";
+		const { length: countMovies } = this.state.movies;
+		classes += countMovies === 0 ? "success" : "info";
+		return classes;
+	}
 	render() {
 		const { length: countMovies } = this.state.movies;
 		if (countMovies === 0) {
 			return (
-				<p className="badge bg-warning text-dark">
+				<p className={this.getBadgeClasess()}>
 					There are no movies in the database.
 				</p>
 			);
 		}
 		return (
 			<div>
-				<p className="badge bg-success">
+				<p className={this.getBadgeClasess()}>
 					Showing {countMovies} movies in the databse
 				</p>
 				<table className="table">
